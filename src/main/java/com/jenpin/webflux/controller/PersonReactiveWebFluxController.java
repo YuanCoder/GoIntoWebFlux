c45d2774f9ad5ab0382fb34d1e8a0a48ba85e511
yuanjiangpeng <1073625256@qq.com>
2018-08-01 10:46:37 +0800

    整合模板

--- src/main/java/com/jenpin/webflux/controller/PersonReactiveWebFluxController.java
+++ src/main/java/com/jenpin/webflux/controller/PersonReactiveWebFluxController.java
@@ -3,6 +3,8 @@ package com.jenpin.webflux.controller;
 import com.jenpin.webflux.handlers.PersonReactiveHandler;
 import com.jenpin.webflux.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -13,31 +15,55 @@ import reactor.core.publisher.Mono;
  * @email: yuan_268311@163.com
  * @description:
  **/
-@RestController
+@Controller
 @RequestMapping(value = "/mogo/person")
 public class PersonReactiveWebFluxController {
 
+    private static final String PERSON_LIST_PATH_NAME = "persons";
+
         @Autowired
         private PersonReactiveHandler personHandler;
+
         @GetMapping(value = "/{id}")
+        @ResponseBody
         public Mono<Person> findPersionById(@PathVariable("id") Long id) {
             return personHandler.findPersonById(id);
         }
+
         @GetMapping()
+        @ResponseBody
         public Flux<Person> findAllPersion() {
             return personHandler.findAllPerson();
         }
+
         @PostMapping()
+        @ResponseBody
         public Mono<Person> savePersion(@RequestBody Person persion) {
             return personHandler.save(persion);
         }
+
         @PutMapping()
+        @ResponseBody
         public Mono<Person> modifyPersion(@RequestBody Person persion) {
             return personHandler.modifyPerson(persion);
         }
+
         @DeleteMapping(value = "/{id}")
         public Mono<Long> deletePersion(@PathVariable("id") Long id) {
             return personHandler.deletePerson(id);
         }
 
+        @GetMapping("/say")
+        public Mono<String> selfIntroduction(final Model model) {
+            model.addAttribute("person",personHandler.findPersonById(1L));
+            String path = "say";
+            return Mono.create(monoSink -> monoSink.success(path));
+        }
+
+        @GetMapping("/list")
+        public String listPage(final Model model) {
+            final Flux<Person> personList = personHandler.findAllPerson();
+            model.addAttribute("personList", personList);
+            return PERSON_LIST_PATH_NAME;
+        }
 }
